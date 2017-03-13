eaba901da16dadb665ae61787adaa9ff975be489
szitguy <luohuafengyue@163.com>
2017-03-13 11:09:17 +0800

    1.version 1.0.0->1.0.1 2.optimize publish script 3.add AllSharePluginExtension to inject qqAppId to res/string 4.optimize replaceWXEntryActivity method

--- src/main/groovy/cn.itguy.allshare/AllSharePlugin.groovy
+++ src/main/groovy/cn.itguy.allshare/AllSharePlugin.groovy
@@ -10,7 +10,19 @@ class AllSharePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+        // 初始化AllSharePlugin扩展参数
+        project.extensions.create(TAG, AllSharePluginExtension);
+
         project.afterEvaluate {
+            def extension = project.extensions.findByName(TAG) as AllSharePluginExtension
+            def qqAppId = extension.qqAppId
+            println("$TAG: Get qqAppId: $qqAppId.")
+//            // 将qqAppId注入android的defaultConfig的manifestholder中。***此方法行不通，资源文件（manifest、res等）合并前，必须执行完单个module的build.gradle中的变量替代***
+//            project.android.defaultConfig.manifestPlaceholders.put("QQ_APP_ID", qqAppId)
+
+            // 改成以res/string的形式注入qqAppId
+            project.android.defaultConfig.resValue("string", "qqAppId", "tencent$qqAppId")
+
             project.android.applicationVariants.each { variant ->
                 def dexTaskName = "transformClassesWithDexFor${variant.name.capitalize()}"
                 def dexTask = project.tasks.findByName(dexTaskName)
@@ -32,6 +44,25 @@ class AllSharePlugin implements Plugin<Project> {
         }
     }
 
+    static File findJar(File inputFile) {
+        File returnFile = null;
+        println("$TAG: findJar---file name=${inputFile.name}")
+        if (inputFile.isDirectory()) {
+            inputFile.listFiles().each { file ->
+                File tempFile = findJar(file)
+                if (tempFile != null) {
+                    returnFile = tempFile
+                }
+            }
+        } else {
+            if (inputFile.name.endsWith(".jar")) {
+                returnFile = inputFile
+            }
+        }
+
+        return returnFile;
+    }
+
     static void replaceWXEntryActivity(def project, def variant, def dexTask) {
         println("$TAG: variant=${variant.name.capitalize()}")
         println("$TAG: variant applicationId=$variant.mergedFlavor.applicationId")
@@ -39,32 +70,38 @@ class AllSharePlugin implements Plugin<Project> {
         Set<File> inputFiles = dexTask.inputs.files.files
         inputFiles.each { inputFile ->
             def path = inputFile.absolutePath
-//            println("$TAG: inputFile path=$path")
+            println("$TAG: inputFile path=$path")
 
-            if (path.contains("allshare") && path.endsWith("jars/classes.jar")) {
-                println("$TAG: processJar=$inputFile.absolutePath")
-                def ruleFile = new File(inputFile.getParent(), "rule.txt")
-                PrintWriter rulePW = new PrintWriter(ruleFile)
-                rulePW.println("rule cn.itguy.allshare.platform.wx.WXEntryActivity ${variant.mergedFlavor.applicationId}.wxapi.WXEntryActivity")
-                rulePW.flush()
-                rulePW.close()
+            // 经过打印观察到，在用版本的gradle插件打包时执行dexTask的输入文件有两个，一个是存放了所有类混淆后的jar包的目录，目录中有个jar包，另一个是个文件，maindexlist.txt记录了要dex处理的class列表
+            // 那么这里我们要做的就是遍历得到的目录中的所有文件，找到jar文件，然后将WXEntryActivity的包名替换过来
+            if (inputFile.isDirectory()) {
+                File file = findJar(inputFile)
+                if (null != file) {
+                    println("$TAG: processJar=$file.absolutePath")
+                    def ruleFile = new File(file.getParent(), "rule.txt")
+                    PrintWriter rulePW = new PrintWriter(ruleFile)
+                    rulePW.println("rule cn.itguy.allshare.platform.wx.WXEntryActivity ${variant.mergedFlavor.applicationId}.wxapi.WXEntryActivity")
+                    rulePW.flush()
+                    rulePW.close()
 
-                def outputFile = new File(inputFile.getParent(), inputFile.name + ".opt")
-                println("$TAG: outputFile=${outputFile.absolutePath}")
+                    def outputFile = new File(file.getParent(), file.name + ".opt")
+                    println("$TAG: outputFile=${outputFile.absolutePath}")
 
-                new Main().process(ruleFile, inputFile, outputFile)
+                    new Main().process(ruleFile, file, outputFile)
 
-                if (inputFile.exists()) {
-                    inputFile.delete()
+                    if (file.exists()) {
+                        file.delete()
+                    }
+
+                    println("$TAG: new jar exists=${file.exists()}")
+
+                    outputFile.renameTo(file)
+
+                    println("$TAG: new jar exists=${file.exists()}")
+                } else {
+                    println("$TAG: jar not found.")
                 }
-
-                println("$TAG: new jar exists=${inputFile.exists()}")
-
-                outputFile.renameTo(inputFile)
-
-                println("$TAG: new jar exists=${inputFile.exists()}")
             }
-
         }
     }
 
---
+++ src/main/groovy/cn.itguy.allshare/AllSharePluginExtension.groovy
@@ -0,0 +1,13 @@
+package cn.itguy.allshare
+
+/**
+ * Created by yelongfei490 on 2017/3/11.
+ */
+class AllSharePluginExtension {
+
+    /**
+     * 插件扩展参数，qq平台的appId
+     */
+    public String qqAppId;
+
+}
