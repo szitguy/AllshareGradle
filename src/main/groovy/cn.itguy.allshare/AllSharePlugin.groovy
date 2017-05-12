4e7625ecdb12000f59dcd24e8afb1c39892f1be2
szitguy <luohuafengyue@163.com>
2017-05-12 15:47:26 +0800

    改成引用jitpack中的android库

--- src/main/groovy/cn.itguy.allshare/AllSharePlugin.groovy
+++ src/main/groovy/cn.itguy.allshare/AllSharePlugin.groovy
@@ -2,8 +2,10 @@ package cn.itguy.allshare
 
 import cn.itguy.BuildConfig
 import com.android.build.gradle.AppExtension
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 
 class AllSharePlugin implements Plugin<Project> {
 
@@ -19,11 +21,21 @@ class AllSharePlugin implements Plugin<Project> {
         project.extensions.create(TAG, AllSharePluginExtension)
         def extension = project.extensions.findByType(AllSharePluginExtension)
 
-        // release时添加jcenter仓库
+        // release时添加jitpack仓库
         if (!BuildConfig.DEBUG) {
-            project.repositories.add(project.getRepositories().jcenter())
+//            project.repositories.add(project.getRepositories().jcenter())
+//            // 添加AllShare Android库依赖
+//            project.dependencies.add("compile", "cn.itguy.allshare:allshare:${BuildConfig.ANDROID_LIB_VERSION}")
+
+            project.repositories.add(project.repositories.maven(new Action<MavenArtifactRepository>() {
+                @Override
+                void execute(MavenArtifactRepository mavenArtifactRepository) {
+                    mavenArtifactRepository.url = 'https://jitpack.io'
+                }
+            }))
             // 添加AllShare Android库依赖
-            project.dependencies.add("compile", "cn.itguy.allshare:allshare:${BuildConfig.ANDROID_LIB_VERSION}")
+            project.dependencies.add("compile", "com.github.szitguy:Allshare:${BuildConfig.ANDROID_LIB_VERSION}")
+
             println("------ANDROID_LIB_VERSION is ${BuildConfig.ANDROID_LIB_VERSION}")
         }
 
